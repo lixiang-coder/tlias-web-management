8ff8d5e02188f6f9b11489eb24296cd2a681d989
xzy <2894702202@qq.com>
2024-06-09 23:00:13 +0800

    feat：新增阿里云OSS文件存储

--- src/main/java/com/itheima/controller/UploadController.java
+++ src/main/java/com/itheima/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.itheima.controller;
 
+import com.itheima.config.AliOSSUtils;
 import com.itheima.pojo.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
@@ -17,15 +19,15 @@ import java.util.UUID;
 @Api(tags = "文件上传相关接口")
 public class UploadController {
 
-    @PostMapping("/upload")
-    @ApiOperation("文件上传")
+    /*@PostMapping("/upload")
+    @ApiOperation("文件上传本地存储")
     public Result upload(String username, Integer age, MultipartFile image) throws IOException {
         log.info("文件上传：{},{},{}", username, age, image);
         // 获取原始文件的名称
         String originalFilename = image.getOriginalFilename();
 
         // 将文件存储在服务器的磁盘目录中(注意：这个路径必须已经存在)
-        /*image.transferTo(new File("D:\\images\\" + originalFilename));*/
+        *//*image.transferTo(new File("D:\\images\\" + originalFilename));*//*
 
         // 构建新的文件名
         String extname = originalFilename.substring(originalFilename.lastIndexOf(".")); //文件扩展名
@@ -33,6 +35,28 @@ public class UploadController {
         image.transferTo(new File("D:\\images\\" + newFileName));
 
         return Result.success();
+    }*/
+
+    @Resource
+    private AliOSSUtils aliOSSUtilsl;
+
+    /**
+     * 文件上传阿里云OSS存储
+     * @param image
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/upload")
+    @ApiOperation("文件上传阿里云OSS存储")
+    public Result upload(MultipartFile image) throws IOException {
+        log.info("文件上传，文件名：{}", image);
+        // 获取原始文件的名称
+        String originalFilename = image.getOriginalFilename();
+
+        // 调用AliOSSUtils工具类
+        String url = aliOSSUtilsl.upload(image);
+        log.info("文件上传完成，文件访问的url：{}", url);
+        return Result.success(url);
     }
 
 }
---
+++ src/test/java/com/itheima/AliOssTest.java
@@ -0,0 +1,61 @@
+package com.itheima;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class AliOssTest {
+    public static void main(String[] args) throws Exception {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-beijing.aliyuncs.com";
+
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tA1aZrCkw9zzS2ts3aB";
+        String accessKeySecret = "skZ96GUFwQ5RLLr9jLFooVzkBwpn5V";
+
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "xzy-web-tlias";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "1.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "D:\\images\\049a2d15-281f-4f89-b475-a5b194592f46.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
+            // 设置该属性可以返回response。如果不设置，则返回的response为空。
+            putObjectRequest.setProcess("true");
+            // 创建PutObject请求。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            // 如果上传成功，则返回200。
+            System.out.println(result.getResponse().getStatusCode());
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+}
