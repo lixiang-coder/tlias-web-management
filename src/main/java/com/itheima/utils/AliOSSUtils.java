87dfe33ec98f0b8df1879d1c3cb44d4e637b5c37
xzy <2894702202@qq.com>
2024-06-10 12:06:09 +0800

    feat：新增参数配置化

--- src/main/java/com/itheima/utils/AliOSSUtils.java
+++ src/main/java/com/itheima/utils/AliOSSUtils.java
@@ -2,6 +2,7 @@ package com.itheima.utils;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
@@ -13,10 +14,17 @@ import java.util.UUID;
 @Component
 public class AliOSSUtils {
 
-    private String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-    private String accessKeyId = "xxx";
-    private String accessKeySecret = "xxx";
-    private String bucketName = "xzy-web-tlias";
+    @Value("${aliyun.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${aliyun.oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aliyun.oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${aliyun.oss.bucketName}")
+    private String bucketName;
 
 
     /**
