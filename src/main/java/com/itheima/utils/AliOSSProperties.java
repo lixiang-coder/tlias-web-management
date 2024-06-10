bbaafcc8b79778d1df7cc3a584186cac50603eae
xzy <2894702202@qq.com>
2024-06-10 12:11:00 +0800

    feat：新增AliOSSProperties类来优化参数配置化

---
+++ src/main/java/com/itheima/utils/AliOSSProperties.java
@@ -0,0 +1,20 @@
+package com.itheima.utils;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/*阿里云OSS相关配置*/
+@Data
+@Component
+@ConfigurationProperties(prefix = "aliyun.oss")
+public class AliOSSProperties {
+    //区域
+    private String endpoint;
+    //身份ID
+    private String accessKeyId ;
+    //身份密钥
+    private String accessKeySecret ;
+    //存储空间
+    private String bucketName;
+}
--- src/main/java/com/itheima/utils/AliOSSUtils.java
+++ src/main/java/com/itheima/utils/AliOSSUtils.java
@@ -2,9 +2,12 @@ package com.itheima.utils;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
 import java.io.*;
 import java.util.UUID;
 
@@ -14,37 +17,70 @@ import java.util.UUID;
 @Component
 public class AliOSSUtils {
 
-    @Value("${aliyun.oss.endpoint}")
-    private String endpoint;
-
-    @Value("${aliyun.oss.accessKeyId}")
-    private String accessKeyId;
-
-    @Value("${aliyun.oss.accessKeySecret}")
-    private String accessKeySecret;
-
-    @Value("${aliyun.oss.bucketName}")
-    private String bucketName;
+    // 这样写太过于麻烦
+//    @Value("${aliyun.oss.endpoint}")
+//    private String endpoint;
+//
+//    @Value("${aliyun.oss.accessKeyId}")
+//    private String accessKeyId;
+//
+//    @Value("${aliyun.oss.accessKeySecret}")
+//    private String accessKeySecret;
+//
+//    @Value("${aliyun.oss.bucketName}")
+//    private String bucketName;
 
 
     /**
      * 实现上传图片到OSS
      */
-    public String upload(MultipartFile file) throws IOException {
+//    public String upload(MultipartFile file) throws IOException {
+//        // 获取上传的文件的输入流
+//        InputStream inputStream = file.getInputStream();
+//
+//        // 避免文件覆盖
+//        String originalFilename = file.getOriginalFilename();
+//        // 构建新的文件名
+//        String fileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+//
+//        // 上传文件到 OSS
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//        ossClient.putObject(bucketName, fileName, inputStream);
+//
+//        // 拼接文件访问路径。eg：https://xzy-web-tlias.oss-cn-beijing.aliyuncs.com/1.jpg
+//        String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
+//
+//        // 关闭ossClient
+//        ossClient.shutdown();
+//
+//        // 把上传到oss的路径返回
+//        return url;
+//    }
+
+
+    //注入配置参数实体类对象
+    @Resource
+    private AliOSSProperties aliOSSProperties;
+
+
+    /**
+     * 实现上传图片到OSS
+     */
+    public String upload(MultipartFile multipartFile) throws IOException {
         // 获取上传的文件的输入流
-        InputStream inputStream = file.getInputStream();
+        InputStream inputStream = multipartFile.getInputStream();
 
         // 避免文件覆盖
-        String originalFilename = file.getOriginalFilename();
-        // 构建新的文件名
+        String originalFilename = multipartFile.getOriginalFilename();
         String fileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
 
         // 上传文件到 OSS
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        ossClient.putObject(bucketName, fileName, inputStream);
+        OSS ossClient = new OSSClientBuilder().build(aliOSSProperties.getEndpoint(),
+                aliOSSProperties.getAccessKeyId(), aliOSSProperties.getAccessKeySecret());
+        ossClient.putObject(aliOSSProperties.getBucketName(), fileName, inputStream);
 
-        // 拼接文件访问路径。eg：https://xzy-web-tlias.oss-cn-beijing.aliyuncs.com/1.jpg
-        String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
+        // 文件访问路径
+        String url =aliOSSProperties.getEndpoint().split("//")[0] + "//" + aliOSSProperties.getBucketName() + "." + aliOSSProperties.getEndpoint().split("//")[1] + "/" + fileName;
 
         // 关闭ossClient
         ossClient.shutdown();
