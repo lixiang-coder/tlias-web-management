eaedc9dea625a29eb721cbdff9e8364234a6c421
xzy <2894702202@qq.com>
2024-06-10 12:20:09 +0800

    feat：新增依赖，优化警告，使yml文件书写有提示

--- src/main/java/com/itheima/utils/AliOSSUtils.java
+++ src/main/java/com/itheima/utils/AliOSSUtils.java
@@ -17,7 +17,9 @@ import java.util.UUID;
 @Component
 public class AliOSSUtils {
 
-    // 这样写太过于麻烦
+    // @Value注解只能一个一个的进行外部属性的注入。
+    // @ConfigurationProperties可以批量的将外部的属性配置注入到bean对象的属性中。(推荐)
+
 //    @Value("${aliyun.oss.endpoint}")
 //    private String endpoint;
 //
