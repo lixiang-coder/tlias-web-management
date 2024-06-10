b1b041b070251c0d51f0cee9e18b5b760dbe0fc0
xzy <2894702202@qq.com>
2024-06-10 09:23:03 +0800

    feat：新增阿里云OSS文件存储

--- src/test/java/com/itheima/AliOssTest.java
+++ src/test/java/com/itheima/AliOssTest.java
@@ -16,8 +16,8 @@ public class AliOssTest {
         String endpoint = "https://oss-cn-beijing.aliyuncs.com";
 
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tA1aZrCkw9zzS2ts3aB";
-        String accessKeySecret = "skZ96GUFwQ5RLLr9jLFooVzkBwpn5V";
+        String accessKeyId = "youraccessKeyId";
+        String accessKeySecret = "youraccessKeySecret";
 
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "xzy-web-tlias";
